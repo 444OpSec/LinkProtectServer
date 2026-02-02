9ee2abb6c07b74ec33bd43297e0f5aacb253747a
PD758 <90046766+PD758@users.noreply.github.com>
2026-02-03 01:39:13 +0300

    fix model/api mismatch

--- app/models.py
+++ app/models.py
@@ -5,12 +5,12 @@ class HealthCheck(BaseModel):
     status: str = "OK"
 
 class UserSettings(BaseModel):
-    allow_get_links_content: bool = Field(..., description="Разрешил ли пользователь получать содержимое ссылки для проверок")
+    allow_get_links_contents: bool = Field(..., description="Разрешил ли пользователь получать содержимое ссылки для проверок")
     link_deep_check: bool = Field(..., description="Проводить ли более длительные, но точные проверки")
 
 class ScanRequest(BaseModel):
     url: str = Field(..., examples=["https://yandex.ru", "http://test.com"], description="Ссылка для проверки")
-    user_settings: UserSettings
+    settings: UserSettings
 
 class ScanResponse(BaseModel):
     result: bool = Field(..., description="Безопасна ли ссылка")
